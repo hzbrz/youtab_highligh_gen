84ce64f7590b941f771fe5a6a451b3cd7ab7da58
Hasan Zahid <hasanthedev@gmail.com>
2018-08-10 18:04:06 -0500

    Getting clips and dl links on other computer

--- twitch_dlr.py
+++ twitch_dlr.py
@@ -11,16 +11,41 @@ headers = {
 
 # https://github.com/amiechen/twitch-batch-loader/blob/master/batchloader.py
 # https://dev.twitch.tv/docs/v5/reference/clips/#get-clip
-# loop through channel names: loltyler1, LCK1, TFBlade, imaqtpie, Trick2g, LLStylez, Yassuo, Shiphtur, C9Sneaky, hashinshin, Gosu, Doublelift, Nightblue3, Voyboy, nalcs, midbeast
+# loop through channel names from credentials.py 
 # loop in the order above and get 1 clip from each channel from that day
 # end loop when the added duration of all vids is above 15mins
 
 
-clip_req = requests.get(
-          "https://api.twitch.tv/kraken/clips/top?channel=loltyler1", 
-           headers=headers
-          ).json()
+vid_dur_in_sec = 0
+counter = 0
+video_duration = 0
 
+while video_duration < 15.0:
+  counter = counter + 1
+  for streamer in credentials.league_names:
+    clip_req = requests.get(
+            "https://api.twitch.tv/kraken/clips/top?channel="+streamer+"&period=day", 
+            headers=headers
+            ).json()
+    if len(clip_req['clips'][counter-1:counter]) != 0:
+      clip = clip_req['clips'][counter-1:counter][0]
+      thumb = clip['thumbnails']['medium']
+      title = clip['title']
+      clip_duration = clip['duration']
+      slice_index = thumb.index('-preview-')
+      twitch_dl_url = thumb[:slice_index] + ".mp4"
+      # pprint.pprint(clip)
+      
+      vid_dur_in_sec = vid_dur_in_sec + clip_duration
+      video_duration = vid_dur_in_sec / 60
+      print(clip_duration, video_duration)
+      print(title)
+      print(twitch_dl_url)
+      # print(counter)
+
+
+
+             
 # clips = clip_req['data']
 
 # for clip in clips:
@@ -29,4 +54,3 @@ clip_req = requests.get(
 #   twitch_dl_url = thumb[:slice_index] + ".mp4"
 #   print(twitch_dl_url)
 
-pprint.pprint(clip_req['clips'])
