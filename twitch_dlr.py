39b26494c4c4c9e4f053579ab68a7783a27b73e5
Hasan Zahid <hasanthedev@gmail.com>
2018-08-09 20:10:36 -0500

    getting the twitch clips

--- twitch_dlr.py
+++ twitch_dlr.py
@@ -2,11 +2,12 @@ import credentials
 import requests, urllib.request,pprint
 
 headers = {
-  'Accept': 'application/vnd.twitchtv.v5+json',
   'Client-ID': credentials.twitch_cid
 }
 
-# getting the game id - league: 497467, fortnite: 33214
-clip_req = requests.get("https://api.twitch.tv/kraken/games/top", headers=headers).json()
+# getting the game id - league: 21779, fortnite: 33214
+#clip_req = requests.get("https://api.twitch.tv/kraken/games/top", headers=headers).json()
+
+clip_req = requests.get("https://api.twitch.tv/helix/clips?game_id=21779&first=5", headers=headers).json()
 
 pprint.pprint(clip_req)
